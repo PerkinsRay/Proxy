1b3f3e67be39135b66a19728cf7bb9d9eef531f7
lxserenade <lixia625@hotmail.com>
2014-11-24 17:30:32 +0800

    http

--- NioServer/src/http.java
+++ NioServer/src/http.java
@@ -3,7 +3,11 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.StringReader;
 import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
+import java.nio.charset.Charset;
+import java.nio.charset.CharsetDecoder;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 
@@ -12,183 +16,180 @@ public class http {
 	
 	private boolean built=false;
 	private int status=0;  //0:undetermined 1:header  2:content-length  3:chunked  4:complete 
-	private ArrayList<Packet> packet=new ArrayList<Packet>();
+	private ArrayList<Packet> packet_list=new ArrayList<Packet>();
 	
-	private head h;
-	private string content;
+	private head head;
+	private Packet current_packet;
+	private String content;
 	private ByteBuffer buffer= ByteBuffer.allocate(1000);
-
+	String http_method[]={"HTTP/1.0","HTTP/1.1","GET","POST","HEAD","PUT","DELETE","TRACE","OPTIONS","PATCH","CONNECT"};
 	
-
+	
+	
+	public Packet get_packet(){
+		if(this.packet_list.size()>0){
+			Packet re=this.packet_list.get(0);
+			this.packet_list.remove(0);
+			return re;
+		}
+		else{
+			return null;
+		}
+	}
+	
+/**********************************
+ * return: 0-uncomplete  1-complete 2-chunked
+ * 
+ *********************************/
 	public int build(byte[] data) throws Exception{
-		///////////////////////////////////////////
-		//PSEUDO
-		
-		
-		if status==0:
-			h.build_head ,//->  0,1 2 3 4
-			if 0:return 
-			if 1:buffer.put(data),status=1,return 1
-		    if 2:head_length=h.getheadlength,getcontentlength(),content=data[head_lenth:contentlength]
-		    		if buffer>contentlength+headlength: status=4;
-		    		if buffer<contentlength:status=2;
-		    if 3:builder_chunk
-		    
-		    if 4:headpacket 
-			
-		if status==1:
-			find "\r\n\r\n"
-			if exists: buffer.put(data[:"\r\n\r\n"])
-			   h.build_head(buffer)
-			   		if 2:
-			   		if 3:
-			   		if 4:
-			if not exists:return
-		
-		if status==2:data+buffer>contentlength:build content
-					else return
-		if status==3:buildcontent:data+buffer//chunk
-		
-		if status==4:packet+=1;clear in buffer;status=0;go to start;
-		
-		
-			
-			if so:
-					status=1;
-					h=this,header.build(buffer);
-					
-					
-				
-				ELSE RETURN;
-		
-					
-	   if  status==1:
-		   if builder.len>3: buffer-3 ->string,if "\r\n\r\n":  
-				
-		
-		builder.put(buffer);
-		String str=new String(builder.array());
-		
-		processing str....   -> set status;
-		
-		
-		if status==1: 是packet｛取出buffer，构建packet，packet.add（）｝
-		if status==2:	
-				
-		if status==4 ，是packet：{取出buffer，构建packet,packet.add();}
-		
-		
-		
-		return packet。length
 		
 		//////////////////////////////////////////
+		if(packet_list.size()!=0){
+			return 1;
+		}
 		
 		try{
-			builder.put(buffer);
-			String str=new String(builder.array());
+			buffer.put(data);
+			String str="";
 			String subs="";
 			String content="";
 			//StringReader sr=new StringReader(str);
 			//BufferedReader br = new BufferedReader(sr);
-		
 			
-			//header
-			if(header_length==-1){
-	
-				header_length=str.indexOf("\r\n\r\n")+4;
-				if(header_length!=-1){
-					
-					if(str.indexOf("Content_Length:")!=-1){
-						subs=str.substring(str.indexOf("Content_Length:")+"Content_Length:".length(),str.length());
-						this.Content_Length=Integer.parseInt(subs.substring(0,subs.indexOf("\r\n")).trim());
-						
-	
-					}
-					
-					if(str.indexOf("Transfer-Encoding:")!=-1){
-						subs=str.substring(str.indexOf("Transfer-Encoding:")+"Transfer-Encoding:".length(),str.length());
-						this.Transfer_Encoding=subs.substring(0,subs.indexOf("\r\n")).trim();
-						
-					}
-					if(str.indexOf("Host:")!=-1){
-						subs=str.substring(str.indexOf("Host:")+"Host:".length(),str.length());
-						this.Host=subs.substring(0,subs.indexOf("\r\n")).trim();
-						
-					}
+			buffer.flip();
+			//System.out.println(buffer.limit());
+			Charset charset = Charset.forName("UTF-8");  
+			CharsetDecoder decoder = charset.newDecoder();  
+            // charBuffer = decoder.decode(buffer);//用这个的话，只能输出来一次结果，第二次显示为空  
+			CharBuffer charBuffer = decoder.decode(buffer.asReadOnlyBuffer()); 
+			str=charBuffer.toString();
+			//System.out.println(str);
+			
+			if(status==0){
+				if(str.length()<4){
+					this.status=0;
+					return 0;
 				}
-	
-			}
-			//end header
-			header=str.substring(0,this.header_length);
-			String current_content=str.substring(this.header_length,builder.position());
-			
-			//System.out.println(current_content);
-			
-			if (this.Content_Length!=-1){
-				if(current_content.length()==this.Content_Length){
-					content=str.substring(this.header_length,builder.position());
-					this.built=true;
-					return 1;
-				}
-			}
-			
-			else if(this.Transfer_Encoding.equals("chunked")){
 				
-				for(int i=0;i<=current_content.length();){
-					String one_chunk=current_content.substring(i,current_content.length());
+				
+				
+				if(Arrays.asList(this.http_method).contains(str.split(" ")[0])){
+					this.status=1;		
+				}
+				else{
+					buffer.clear();
+					this.status=0;
+					return 0;
+				}
+				
+			}
+			
+			if(status==1){
+				
+				if(str.indexOf("\r\n\r\n")!=-1){
+					subs=str.substring(0,str.indexOf("\r\n\r\n"));
+					this.head=new head(subs);
 					
-					
-					String chunk_num=one_chunk.substring(0,one_chunk.indexOf("\r\n"));
-					int j=Integer.parseInt(chunk_num)+chunk_num.length()+4;
-					
-					if(j!=5){
-					this.chunks.add(current_content.substring(i,i+j-1));
-					
+					if(!(this.head.get("content-length").equals("none"))){
+						this.current_packet=new Packet(this.head,"");
+						this.status=2;
+					}
+					else if((this.head.get("transfer-encoding").toLowerCase().equals("chunked"))){
+						this.current_packet=new Packet(this.head,"");
+						this.status=3;
+						
 					}
 					else{
-						this.built=true;
-						this.chunks.add("0");
+						this.status=4;
+						this.current_packet=new Packet(this.head,"");
+						this.packet_list.add(this.current_packet);
+						buffer.clear();
 						return 1;
 					}
-					
-					i+=j;
+
 				}
-	
-				
+				else{
+					this.status=1;
+					return 0;
+				}
 			}
-	
 			
-			return 0;
+			if(status==2){
+				int content_length=Integer.parseInt(this.head.get("content-length").trim());
+				
+				subs=str.substring(str.indexOf("\r\n\r\n"),str.length());
+				buffer.position(str.indexOf("\r\n\r\n"));
+				if(subs.length()>=content_length){
+					
+					this.current_packet=new Packet(this.head,subs.substring(0, content_length-1));
+					this.packet_list.add(this.current_packet);
+					buffer.position(buffer.position()-content_length);
+					return 1;
+				}
+				
+				else{
+					buffer.position(buffer.position()-subs.length());
+					return 0;
+				}
 			}
-		catch(Exception e){
-			this.built=false;
-			return 0;
+			
+			if(status==3){
+				//str=new String(buffer.array());
+				
+				String current_content=str.substring(str.indexOf("\r\n\r\n")+4,str.length());
+				String tmp="";
+				buffer.position(str.indexOf("\r\n\r\n"));
+				
+				for(int i=0;i<=current_content.length();){
+ 				    tmp=current_content.substring(i,current_content.length());
+ 					String chunk_num=tmp.substring(0,tmp.indexOf("\r\n"));
+ 					System.out.println(chunk_num);
+ 					int j=Integer.parseInt(chunk_num.trim())+chunk_num.length()+4;
+ 					
+ 					System.out.println(i+" "+j+" "+current_content.length());
+ 					if((i+j)>current_content.length()){
+ 						buffer.position(buffer.position()-current_content.length());
+ 						return 0;}
+ 					
+ 					if(Integer.parseInt(chunk_num.trim())!=0){
+ 						
+ 						this.current_packet.add_chunk(current_content.substring(i,i+j-1));
+ 						buffer.position(buffer.position()-i+j-1);
+ 					}
+ 					else{
+ 						status=4;
+ 						System.out.println("ge");
+ 						this.current_packet.add_chunk("0");
+ 						System.out.println("geqw");
+ 						this.packet_list.add(current_packet);
+ 						return 1;
+ 					}
+ 					
+ 					i+=j;
+ 				}
+				
+				return 2;
+ 				
+ 			}
+ 	
+			
+			else {
+				buffer.clear();
+				return 0;
+			}
+				
+			}catch(Exception e){
+				System.out.println(e.toString());
+				return 0;
 			}
 	}
-
-
-
-	public String getchunk(){
-			String res=this.chunks.get(0);
-			this.chunks.remove(0);
-			return res;
-	}
-
-	public byte[] getheader(){
-		return this.header.getBytes();
-	}
-
-	public byte[] getcontent(){
-		return this.content.getBytes();
-	}
-	public void setConnection(String conn){
-		this.header=this.header.replaceAll("(Connection:).*?((\\r\\n))","$1"+conn+"$2");
-	}
-	
+			
+			
+			
 
 	
 	public static void main(String args[]) throws Exception{
-		String s="Accept:image/webp,*/*;q=0.8\r\nConnection:keep-alive\r\nHost:ww1.sinaimg.cn\r\nTransfer-Encoding:chunked\r\nAccept-Encoding:gzip,deflate,sdch\r\nAccept-Language:en-US,en;q=0.8\r\nCache-Control:max-age=0\r\nConnection:keep-alive\r\nHost:img.t.sinajs.cn\r\nIf-Modified-Since:Wed, 29 Oct 2014 08:58:53 GMT\r\nIf-None-Match:\"5450ac4d-e710\"\r\nReferer:http://weibo.com/u/1710751805/home?topnav=1&wvr=5\r\nUser-Agent:Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/38.0.2125.111 Safari/537.36\r\n\r\n4\r\nxxxx\r\n3\r\nxxx\r\n1\r\nx\r\n0\r\n\r\n";
+		String s="HTTP/1.1 200 OK\r\nAccept:image/webp,*/*;q=0.8\r\nConnection:keep-alive\r\nHost:ww1.sinaimg.cn\r\nTransfer-Encoding:chunked\r\ni/537.36\r\n\r\n4\r\nxxxx\r\n3\r\nxxx\r\n1\r\nx\r\n0\r\n\r\n";
 		
 		StringReader sr=new StringReader(s);
 		BufferedReader br = new BufferedReader(sr);
@@ -198,24 +199,79 @@ public class http {
 		
 		http h=new http();
 		h.build(s.getBytes());
-		h.setConnection("alive");
-		System.out.println(new String(h.getheader()));
-		System.out.println(h.Content_Length);
-		System.out.println(h.built);
-		System.out.println(h.getchunk());
-		System.out.println(h.getchunk());
-		System.out.println(h.getchunk());
+		//h.build(s.getBytes());
+		//h.build(s.getBytes());
+		
+		System.out.println(h.get_packet().get_head().toSting());
+		
+		//System.out.println(h.build(s.getBytes()));
+		System.out.println(h.head.get("hoSt"));
+		System.out.println(h.head.get("transfer-encoding"));
+		
+		//h.setConnection("alive");
+		
 
 	}
 	
 public class Packet{
-	private ArrayList<String[]> header_list=new ArrayList<String[]>();
-	private List<String> chunks=new ArrayList<String>();
+	private head head;
+	private ArrayList<String> chunks;
+	private String content;
 	//private String type=""; //POST GET HTTP.....
 	
+	public Packet(head head,String content){
+		this.head=head;
+		this.chunks=new ArrayList<String>();
+		this.content=content;	
+	}
 	
+	public void add_chunk(String chunk){
+		chunks.add(chunk);	
+	}
+	
+	public head get_head(){
+		return this.head;
+	}
+	
+	
+}
 
-
+public class head{
+	private ArrayList<String[]> header_list=new ArrayList<String[]>();
+	
+	public head(String s){
+		String[] tmp=s.trim().split("\r\n");
+	
+		for(int i=0;i<tmp.length;i++){
+			this.header_list.add(tmp[i].split(":"));
+			
+		}	
+	}
+	
+	public String get(String args){
+		args=args.trim().toLowerCase();
+		for(int i=0;i<header_list.size();i++){
+			if(header_list.get(i)[0].trim().toLowerCase().equals(args)){
+				return header_list.get(i)[1].trim();
+			}
+		}
+		return "none";
+	}
+	
+	public String toSting(){
+		String re="";
+		for(int i=0;i<header_list.size();i++){
+			if(header_list.get(i).length==1){
+				re+=header_list.get(i)[0]+"\r\n";
+			}
+			else{
+				re+=header_list.get(i)[0]+":"+header_list.get(i)[1]+"\r\n";			
+		
+			}
+		}
+		return re+"\r\n";
+		
+	}
 }
 
 
